0da8c8086ff6a910e8baf47304cf37b13845827b
Bell <bobwizardlad@Gmail.com>
2024-09-26 11:51:44 -0500

    Item Placeholder Sprites
    Added more items and placeholder for the item textures.

--- Evil Farming Game/Tests/InventoryItemTester.gd
+++ Evil Farming Game/Tests/InventoryItemTester.gd
@@ -5,14 +5,24 @@ extends Node2D
 
 @export var inventory: Inventory
 @onready var carrot: Item = load("res://Assets/Resources/Items/carrot.tres")
-@onready var godot: Item = load("res://Assets/Resources/Items/godot.tres")
+@onready var fish: Item = load("res://Assets/Resources/Items/fish.tres")
+@onready var tomato: Item = load("res://Assets/Resources/Items/tomato.tres")
 
 # Called when the node enters the scene tree for the first time.
 func _on_button_pressed() -> void:
 	inventory.add_item(carrot)
 
-func _on_button_3_pressed() -> void:
-	inventory.add_item(godot)
-
 func _on_button_2_pressed() -> void:
 	inventory.remove_item(carrot)
+
+func _on_button_3_pressed() -> void:
+	inventory.add_item(fish)
+
+func _on_button_4_pressed() -> void:
+	inventory.remove_item(fish)
+
+func _on_button_5_pressed() -> void:
+	inventory.add_item(tomato)
+
+func _on_button_6_pressed() -> void:
+	inventory.remove_item(tomato)
